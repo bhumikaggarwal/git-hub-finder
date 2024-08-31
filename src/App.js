d4bc429f5c119ee3d5c7c51af326e695927aa051
bhumikaaggarwal <aggarwalbhumika99@gmail.com>
2024-08-31 09:46:46 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import  './index.css' ; 
+import {BrowserRouter as Router , Route,Routes} from 'react-router-dom' ;
+//importing components - layout
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+//importing route,pages
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import User from './pages/User';
+
+import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
+
+import Alert from './components/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GithubProvider>
+    <AlertProvider>
+    
+    <Router>
+    <div className='flex flex-col justify-between  '>
+    <Navbar />
     </div>
+    <Alert />
+    <main className='container mx-auto px-3 pb-12 '>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/user/:login' element={<User />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </main>
+
+    <Footer />
+    </Router>
+    </AlertProvider>
+    </GithubProvider>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/UserSearch'
+
+
+function Home() {
+  return (
+    <div >
+    <UserResults />
+    <UserSearch />
+    
+    </div>
+  )
+}
+
+export default Home
